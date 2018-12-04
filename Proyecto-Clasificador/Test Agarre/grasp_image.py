2a7987c0333951aca94910d76396f5527798d84e
Angelo <angelomontano42@gmail.com>
2018-12-03 22:52:35 -0300

    correccion carga de modelo 3/12

--- Proyecto-Clasificador/Test Agarre/grasp_image.py
+++ Proyecto-Clasificador/Test Agarre/grasp_image.py
@@ -46,7 +46,71 @@ def drawRectangle(I, h, w, t, gsize=300):
 	print len(im_points)
 	return I_temp
 
+##############################Agregado 3/12 (Probar antes)
+nbatches=0
+batchsize=0
+nbest=1
+def init_model():
+    model_path='models/Grasp_model'
+    nsamples=250
 
+    max_batchsize = 128
+    gpu_id=-1
+    global nbatches,batchsize
+    ## Set up model
+    if nsamples > max_batchsize:
+        batchsize = max_batchsize
+        nbatches = int(nsamples/max_batchsize) + 1
+    else:
+        batchsize = nsamples
+        nbatches = 1
+
+    print('Loading grasp model')
+    st_time = time.time()
+    G = grasp_obj(model_path, gpu_id)
+    G.BATCH_SIZE = batchsize
+    G.test_init()
+
+    return G
+
+def prediccion_grasp(I,G):
+    gscale=0.3
+    imsize = max(I.shape[:2])
+    gsize = int(gscale*imsize) # Size of grasp patch
+
+    P = Predictors(I,G)
+
+    fc8_predictions=[]
+    patch_Hs = []
+    patch_Ws = []
+
+    print('Predicting on samples')
+    st_time = time.time()
+    for _ in range(nbatches):
+        P.graspNet_grasp(patch_size=gsize, num_samples=batchsize);
+        fc8_predictions.append(P.fc8_norm_vals)
+        patch_Hs.append(P.patch_hs)
+        patch_Ws.append(P.patch_ws)
+
+    fc8_predictions = np.concatenate(fc8_predictions)
+    patch_Hs = np.concatenate(patch_Hs)
+    patch_Ws = np.concatenate(patch_Ws)
+
+    r = np.sort(fc8_predictions, axis = None)
+    r_no_keep = r[-nbest]
+
+    for pindex in range(fc8_predictions.shape[0]):
+        for tindex in range(fc8_predictions.shape[1]):
+            if fc8_predictions[pindex, tindex] < r_no_keep:
+                continue
+            else:
+                I = drawRectangle(I, patch_Hs[pindex], patch_Ws[pindex], tindex, gsize)
+
+    print('displaying image')
+    #cv2.imshow('image',I)
+    print 'type ',type(I)
+
+##############################################
 #python grasp_image.py --im ./a5.jpg --model ./models/Grasp_model --nbest 5 --nsamples 250 --gscale 0.234 --gpu -1
 
 def get_points():
